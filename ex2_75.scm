a271a07301d59350283b66c639105be6c00c4ce2
Mohd Furquan <furquan2011@gmail.com>
2020-06-12 10:11:35 +0530

    ex2_79

---
+++ ex2_75.scm
@@ -0,0 +1,10 @@
+(define (make-from-mag-ang r theta)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos theta)))
+	  ((eq? op 'imag-part) (* r (sin theta)))
+	  ((eq? op 'magnitude) r)
+	  ((eq? op 'angle)     theta)
+	  (else (error "Unknown op - MAKE-FROM-MAG-ANG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))
---
+++ test2_79.scm
@@ -0,0 +1,9 @@
+(load "ex2_79")
+(install-rectangular-package)
+(install-polar-package)
+(install-complex-package)
+
+(define z1 (make-from-real-imag 1 2))
+(define z2 (make-from-real-imag 3 0))
+(define z3 (make-from-mag-ang 0 2))
+(define z4 (make-from-mag-ang 3 0))
